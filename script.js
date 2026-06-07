08459665584c701ea1647e12b9446fff746d4c21
Snehitha Barukula <barukula.s@northeastern.edu>
2026-06-07 01:38:57 -0400

    Add SmartLink interactive image

--- script.js
+++ script.js
@@ -1,7 +1,5 @@
 const menuBtn = document.getElementById("menuBtn");
 const mobileMenu = document.getElementById("mobileMenu");
-const nextBtn = document.getElementById("nextBtn");
-const prevBtn = document.getElementById("prevBtn");
 
 menuBtn.addEventListener("click", () => {
   mobileMenu.classList.toggle("active");
@@ -11,17 +9,16 @@ const slides = [
   {
     title: "TATA Tiago",
     price: "Price starts at ₹4.69 Lakh*",
-    tagline: "Just WOW!"
+    tagline: "Just WOW!",
+    image: "images/desktop-tiago-new.avif",
+    link: "https://interactivity.studio/ugc/barukula_snehitha8870/demo-tiago"
   },
   {
     title: "TATA Punch",
     price: "Price starts at ₹6.13 Lakh*",
-    tagline: "Go Anywhere!"
-  },
-  {
-    title: "TATA Nexon",
-    price: "Price starts at ₹8.00 Lakh*",
-    tagline: "New Forever!"
+    tagline: "Go Anywhere!",
+    image: "images/d-home-new-punch-banner-new.avif",
+    link: "#"
   }
 ];
 
@@ -30,11 +27,18 @@ let currentSlide = 0;
 const title = document.querySelector(".content-box h1");
 const price = document.querySelector(".content-box p");
 const tagline = document.querySelector(".content-box h3");
+const heroImage = document.querySelector(".hero-image");
+const heroLink = document.querySelector(".hero-image-link");
+
+const nextBtn = document.getElementById("nextBtn");
+const prevBtn = document.getElementById("prevBtn");
 
 function updateSlide() {
   title.textContent = slides[currentSlide].title;
   price.textContent = slides[currentSlide].price;
   tagline.textContent = slides[currentSlide].tagline;
+  heroImage.src = slides[currentSlide].image;
+  heroLink.href = slides[currentSlide].link;
 }
 
 nextBtn.addEventListener("click", () => {
